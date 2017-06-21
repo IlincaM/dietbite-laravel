dba05c7ee870906f5af0f2089fbbf265359af6d6
IlincaM <m.ilinca92@gmail.com>
2017-06-21 17:26:07 +0300

    updated

--- resources/views/layouts/form-bmr.blade.php
+++ resources/views/layouts/form-bmr.blade.php
@@ -62,3 +62,12 @@ echo '</pre>';
 <hr>
 {{ Form::submit('Calculate') }}
 {!! Form::close() !!}
+
+@if ($errors->any())
+<ul class="alert alert-danger">
+    @foreach ($errors->all() as $error)
+    <li> {{ $error }}</li>
+    @endforeach
+</ul>
+
+@endif
