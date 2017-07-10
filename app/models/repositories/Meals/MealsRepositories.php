9f6d5e6c66c420a70e786c7d8a4193e216bc5026
Ilinca <m.ilinca92@gmail.com>
2017-07-10 23:01:39 +0300

    updated

--- app/models/repositories/Meals/MealsRepositories.php
+++ app/models/repositories/Meals/MealsRepositories.php
@@ -36,21 +36,21 @@ class MealsRepositories {
 //$food_feed = file_get_contents($url);
 //echo $food_feed;
         $sessionCaloriesPerWeek = Session::get('result');
-        reset($sessionCaloriesPerWeek);
-        while (current($sessionCaloriesPerWeek)) {
-            echo current($sessionCaloriesPerWeek); // print the current value of array
-            echo '<br>';
-            next($sessionCaloriesPerWeek);
-        }
-
-        $getBreakfast = DB::table('ABBREV')
+        foreach ($sessionCaloriesPerWeek as $keySessionCaloriesPerWeek =>$valueSessionCaloriesPerWeek){
+            for($i=0;$i<=$keySessionCaloriesPerWeek;$i++){
+              $getBreakfast[$i] = DB::table('ABBREV')
                 ->join('FOOD_DES', 'ABBREV.NDB_No', '=', 'FOOD_DES.NDB_No')
                 ->join('FD_GROUP', 'FOOD_DES.FdGrp_Cd', '=', 'FD_GROUP.FdGrp_Cd')
-                ->where('FD_GROUP.FdGrp_Cd', '0100')
-                ->where('ABBREV.Energ_Kcal', '<=', '300')
+//                ->where('FD_GROUP.FdGrp_Cd', '0100')
+                ->where('ABBREV.Energ_Kcal', '<=', $valueSessionCaloriesPerWeek)
                 ->inRandomOrder()
-                ->take(2)
-                ->get();
+                ->take(3)
+              ->get();
+            }
+
+        }
+
+   
         return $getBreakfast;
     }
 
