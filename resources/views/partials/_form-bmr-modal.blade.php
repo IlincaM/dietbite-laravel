452bc4aa4d32a99b2a5a202a1dca95a89b711743
IlincaM <m.ilinca92@gmail.com>
2017-09-08 14:53:35 +0300

    changes

--- resources/views/partials/_form-bmr-modal.blade.php
+++ resources/views/partials/_form-bmr-modal.blade.php
@@ -1,4 +1,4 @@
-s<div class="modal fade" tabindex="-1" role="dialog" id='bmrcalcultaor'>
+<div class="modal fade" tabindex="-1" role="dialog" id='bmrcalcultaor'>
     <div class="modal-dialog" role="document">
         <div class="modal-content">
 
@@ -8,140 +8,148 @@ s<div class="modal fade" tabindex="-1" role="dialog" id='bmrcalcultaor'>
                 <h4 class="modal-title"><i class="fa fa-calculator" aria-hidden="true"></i>BMR Calculator</h4>
             </div>
             <div class="modal-body">
-                <div class="col-md-4 your-age-text">
-                    {{ Form::label('age', 'Your age*') }}
+                <div class="row row-border">
+                    <div class="col-md-4 your-age-text">
+                        {{ Form::label('age', 'Your age*') }}
 
-                </div>
-                <div class="col-md-8 your-age-input">
-                    <input id="age"name="age" type="text" placeholder="25" >
-                    <span id="age_error"></span>
-                </div>
-
-
-                <br>
-                <div class="col-md-4 your-age-text">
-
-                    {{ Form::label('weight', 'Your weight*') }}   
-                </div>
-                <div class="col-md-8 your-age-input">
-
-                    <input name="weight" id="weight" placeholder="kg" type="text">
-                    <span id="weight_error"></span>
-
-                </div>
-                <br>
-                <div class="col-md-4 your-age-text">
-
-                    {{ Form::label('goal_weight', 'Your desired weight*') }}
-                </div>
-                <div class="col-md-8 your-age-input">
-
-                    <input name="goal_weight" id="goal_weight" placeholder="kg" type="text">
-                    <span class="goal_weight-span">*has to be smaller than your weight</span>
-
-                    <span id="goal_weight_error"></span>
-                </div>
-                <br>
-                <div class="col-md-4 your-age-text">
-                    {{ Form::label('height', ' Your height*') }}
-                </div>
-                <div class="col-md-8 your-age-input">
-                    <input name="height" id="height" type="text" placeholder="cm">
-                    <span id="height_error"></span>
+                    </div>
+                    <div class="col-md-8 your-age-input">
+                        <input id="age"name="age" type="text" placeholder="25" >
+                        <span id="age_error"></span>
+                    </div>
                 </div>
                 <hr>
-                <br>
-                <hr>
-                <div class="col-md-4 your-age-text gender-style">
+                <div class="row">
+                    <div class="col-md-4 your-age-text">
 
-                    {{ Form::label('gender', 'Gender') }}
-                </div>
-                <div class="col-md-8 col-gender">
-                    <div class="col-md-3">
-                        {{ Form::label('female', 'female') }}
+                        {{ Form::label('weight', 'Your weight*') }}   
+                    </div>
+                    <div class="col-md-8 your-age-input">
+
+                        <input name="weight" id="weight" placeholder="kg" type="text">
+                        <span id="weight_error"></span>
 
                     </div>
-                    <div class="col-md-3 text-right">
-                        {{ Form::radio('gender', 'female') }}
-
-                    </div>
-                    <div class="col-md-3">
-                        {{ Form::label('male', 'male') }}
-
-                    </div>
-                    <div class="col-md-3  text-right">
-                        {{ Form::radio('gender', 'male') }}
-
-                    </div>
-                    <span id="gender_error"></span>
-                </div>
-                <br>
-                <hr>
-                <div class="col-md-4 your-age-text">
-
-                    {{ Form::label('activity', 'Activiy Level') }}
-                </div>
-                <div class="col-md-8">
-                    @foreach ($bmrActivity as $bmrActivityValues)
-                    <div class="col-md-6">
-                        {{ Form::label('activityLevel', 'Sedentary') }}
-                        {{ Form::radio('activityLevel', $bmrActivityValues["sedentary"]) }}
-                        {{ Form::label('activityLevel', 'Light Activity') }}
-                        {{ Form::radio('activityLevel', $bmrActivityValues["light_activity"]) }}
-                    </div>
-                    <div class="col-md-6">
-
-                        {{ Form::label('activityLevel', ' Active') }}
-                        {{ Form::radio('activityLevel', $bmrActivityValues["normal_active"]) }}
-                        {{ Form::label('activityLevel', ' Very Active ') }}
-                        {{ Form::radio('activityLevel', $bmrActivityValues["very_active"]) }}
-                    </div>
-
-                    @endforeach
-                    <span id="activityLevel_error"></span>
                 </div>
                 <hr>
-                <br>
-                <hr>
-                <div class="col-md-4 your-age-text">
+                <div class="row">
+                    <div class="col-md-4 your-age-text">
 
-                    {{ Form::label('exercise', 'Exercise Level') }}
-                </div>
-                <div class="col-md-8">
-
-                    @foreach ($bmrExercise as $bmrExerciseValues)
-                    <div class="col-md-6">
-
-                        {{ Form::label('exerciseLevel', 'None') }}
-                        {{ Form::radio('exerciseLevel', $bmrExerciseValues["none"]) }}
-                        <br>
-                        {{ Form::label('exerciseLevel', 'Light') }}
-                        {{ Form::radio('exerciseLevel', $bmrExerciseValues["light"]) }}
-                        <br>
-                        {{ Form::label('exerciseLevel', 'Moderate') }}
-                        {{ Form::radio('exerciseLevel', $bmrExerciseValues["moderate"]) }}
+                        {{ Form::label('goal_weight', 'Your desired weight*') }}
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-8 your-age-input">
 
-                        {{ Form::label('exerciseLevel', ' Difficult ') }}
-                        {{ Form::radio('exerciseLevel', $bmrExerciseValues["difficult"]) }}
-                        <br>
-                        {{ Form::label('exerciseLevel', 'Intense') }}
-                        {{ Form::radio('exerciseLevel', $bmrExerciseValues["intense"]) }}
+                        <input name="goal_weight" id="goal_weight" placeholder="kg" type="text">
+                        <span class="goal_weight-span">*has to be smaller than your weight</span>
+
+                        <span id="goal_weight_error"></span>
                     </div>
-                    @endforeach
-                    <span id="exerciseLevel_error"></span>
                 </div>
-                <br>
                 <hr>
+                <div class="row">
+                    <div class="col-md-4 your-age-text">
+                        {{ Form::label('height', ' Your height*') }}
+                    </div>
+                    <div class="col-md-8 your-age-input">
+                        <input name="height" id="height" type="text" placeholder="cm">
+                        <span id="height_error"></span>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-md-4 your-age-text gender-style">
 
+                        {{ Form::label('gender', 'Gender') }}
+                    </div>
+                    <div class="col-md-8 col-gender">
+                        <div class="col-md-3">
+                            {{ Form::label('female', 'female') }}
+
+                        </div>
+                        <div class="col-md-3 text-right">
+                            {{ Form::radio('gender', 'female') }}
+
+                        </div>
+                        <div class="col-md-3">
+                            {{ Form::label('male', 'male') }}
+
+                        </div>
+                        <div class="col-md-3  text-right">
+                            {{ Form::radio('gender', 'male') }}
+
+                        </div>
+                        <span id="gender_error"></span>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-md-4 your-age-text">
+
+                        {{ Form::label('activity', 'Activiy Level') }}
+                    </div>
+                    <div class="col-md-8">
+                        @foreach ($bmrActivity as $bmrActivityValues)
+                        <div class="col-md-6">
+                            {{ Form::label('activityLevel', 'Sedentary') }}
+                            {{ Form::radio('activityLevel', $bmrActivityValues["sedentary"]) }}
+                            {{ Form::label('activityLevel', 'Light Activity') }}
+                            {{ Form::radio('activityLevel', $bmrActivityValues["light_activity"]) }}
+                        </div>
+                        <div class="col-md-6">
+
+                            {{ Form::label('activityLevel', ' Active') }}
+                            {{ Form::radio('activityLevel', $bmrActivityValues["normal_active"]) }}
+                            {{ Form::label('activityLevel', ' Very Active ') }}
+                            {{ Form::radio('activityLevel', $bmrActivityValues["very_active"]) }}
+                        </div>
+
+                        @endforeach
+                        <span id="activityLevel_error"></span>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-md-4 your-age-text">
+
+                        {{ Form::label('exercise', 'Exercise Level') }}
+                    </div>
+                    <div class="col-md-8">
+
+                        @foreach ($bmrExercise as $bmrExerciseValues)
+                        <div class="col-md-6">
+
+                            {{ Form::label('exerciseLevel', 'None') }}
+                            {{ Form::radio('exerciseLevel', $bmrExerciseValues["none"]) }}
+                            <br>
+                            {{ Form::label('exerciseLevel', 'Light') }}
+                            {{ Form::radio('exerciseLevel', $bmrExerciseValues["light"]) }}
+                            <br>
+                            {{ Form::label('exerciseLevel', 'Moderate') }}
+                            {{ Form::radio('exerciseLevel', $bmrExerciseValues["moderate"]) }}
+                        </div>
+                        <div class="col-md-6">
+
+                            {{ Form::label('exerciseLevel', ' Difficult ') }}
+                            {{ Form::radio('exerciseLevel', $bmrExerciseValues["difficult"]) }}
+                            <br>
+                            {{ Form::label('exerciseLevel', 'Intense') }}
+                            {{ Form::radio('exerciseLevel', $bmrExerciseValues["intense"]) }}
+                        </div>
+                        @endforeach
+                        <span id="exerciseLevel_error"></span>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
                 <div class="col-md-4"></div>
                 <div class="col-md-8">
                     <input name="submit" type="submit" class="submitCalculator btn btn-success" value="Calculate">
                     {!! Form::close() !!}
 
                 </div>
-                <div class="bmr-result-div">
+                </div>
+                <div class=" row bmr-result-div">
+                    
                     <div class="col-md-4 col-Cpd">
                         <div>Recommended CPD</div>
                     </div>
